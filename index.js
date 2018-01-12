2b5194b8e89052e456aa4e7890ebfbe020481df9
megha.m <megha.m@github.com>
2018-01-12 11:31:27 +0530

    Callbacks And Error Handling
    Node Callbacks And Error Handling

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ var rect = require('./rectangle');
 function solveReact(l,b) {
     console.log(l,b);
 
-    if(l<=0 || b<=0){
-        console.log("Dimensions should be greater than zero"+ l+b);
-    }
-    else {
-        console.log("Area of Rect "+ rect.area(l,b));
-        console.log("Perimeter of rect "+ rect.perimeter(l,b));
-    }
+    rect(l,b,(err,rectangle) => {
+        if(err) {
+            console.log("ERROR; ", err.message)
+        }
+        else {
+            console.log("l :"+ l + "b: "+ b+ " Area: "+ rectangle.area())
+            console.log("l :" + l + "b: " + b + " Permieter: " + rectangle.perimeter())
+        }
+    });
+    console.log("After calling rectangle")
+
 }
-
-
 solveReact(1,2);
 solveReact(0,5);
 solveReact(4,-1);
